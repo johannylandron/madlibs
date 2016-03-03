f9cbc7274ad363c94f5794972853a4d9ee285776
johannylandron <johannys7864@tywls.org>
2016-03-03 21:47:29 +0000

    fixed bugs and added words

--- madlibs.js
+++ madlibs.js
@@ -1,15 +1,18 @@
 function generateMadlib() {
   
   // This is the starting story
-  var story = "Welcome to ScriptEd! Our mascot is the PLURAL_NOUN.  We love to VERB! ";
+  var story = "Welcome to ScriptEd! Our mascot is the PLURAL_NOUN.  We love to VERB! Sometimes we even verb-2. Do you like to verb-3  ";
   
 
   var inputValue = document.getElementById("plural-noun-1").value;
   story = story.replace("PLURAL_NOUN", inputValue);
   
-  inputValue = document.getElementById("verb").value;
+  inputValue = document.getElementById("verb-1").value;
   story = story.replace("VERB", inputValue);
-  
+  inputValue = document.getElementById("verb-2").value;
+  story = story.replace("verb-2", inputValue);
+  inputValue = document.getElementById("verb-3").value;
+  story = story.replace("verb-3", inputValue);
   // This writes the story when the user clicks the "Generate MadLib" button"
   document.getElementById("result").innerHTML = story;
 }
